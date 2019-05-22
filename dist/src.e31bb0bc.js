c87e566e7cab5bc33d0a98737156a633fd4de58c
Joab <jlsn1@ifal.edu.br>
2019-05-22 15:50:45 -0300

    BabySteps: remove diretório .cache

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -25908,7 +25908,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60345" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60593" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
